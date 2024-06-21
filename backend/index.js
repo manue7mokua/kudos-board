316b435b94753a3ed1bdd2a02a19342205fce962
Emmanuel Onchweri Mokua <manue7mokua@meta.com>
2024-06-21 00:14:55 -0700

    Fetching form data and querying the backend to update it with new board data

--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,33 @@
 const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
+var cors = require('cors');
 
 const express = require('express');
 const app = express();
 const PORT = 3000;
 
-app.use(express.json());
+app.use(cors())
+app.use(express.json({ extended: false }));
+
 
 app.get("/boards", async (req, res) => {
     const boards = await prisma.boards.findMany();
     res.json(boards)
-    console.log(res.json(boards))
 });
+
+app.post("/boards", async (req, res) => {
+    const {title, category, imageUrl, author} = req.body;
+    const newBoard = await prisma.boards.create({
+        data: {
+            title,
+            category,
+            imageUrl,
+            author
+        }
+    });
+    res.json(newBoard);
+});
+
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`)
---
+++ backend/prisma/migrations/20240621062014_init_boards/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "Boards" ALTER COLUMN "imageUrl" DROP NOT NULL;
